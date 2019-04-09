c5924a378b911d1f4f20d0c9e85611666e2959ea
Woody Maddox <woody.e.maddox@gmail.com>
2019-04-08 21:33:15 -0500

    added domstring

--- javascripts/componeants/store.js
+++ javascripts/componeants/store.js
@@ -1,10 +1,16 @@
 
+
+import book from '../helpers/book.js';
 import util from '../helpers/util.js';
 
 
 const makeStore = () => {
- let domString = '<h3>Help</h3>';
-util.printToDom('store-container', domString);
+ const bookInfo = book.getBook();
+ let domString = '<h2>Our Only Book</h2>';
+ domString += `<h3>${bookInfo.price}</h3>`;
+ domString += `<img src=${bookInfo.image} alt="book cover">`;
+ domString += `<button class='btn btn-danger'>Add To Cart</button>`
+ util.printToDom('store-container', domString);
 };
 
 export default {makeStore}; 
